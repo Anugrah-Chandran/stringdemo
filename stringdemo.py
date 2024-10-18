6f536fb71fa7d2942b162932cdbd11a07d9bebad
Anugrah-Chandran <anugrahchandran2028@cs.sjcetpalai.ac.in>
2024-10-18 15:10:30 +0530

    Add files via upload

---
+++ stringdemo.py
@@ -0,0 +1,13 @@
+'''
+Author:Anugrah Chandran
+Date:18-10-24
+Description:Python program that stores a string in a variable.
+ Extract a specific part of the string (substring) and then concatenate it with another
+  string. Finally, display the new string.
+'''
+string1="Hello, World"
+string2=string1[7:12]
+print(string2+" Everyone!")
+
+
+
